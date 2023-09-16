3e830db7e727e1f454dea09d0d82d49bdbda1719
Zhou Jiale <zjl256904767@outlook.com>
2023-09-16 20:09:11 +0800

    线性表

---
+++ CodeProjects/02dataStructure/2Linearlist/2Linearlist/Linearlist.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 顺序表的定义
+	// 静态
+#define MaxSize 50
+typedef struct {
+	//ElemType data[MaxSize];
+	int data[MaxSize];
+	int length;
+}SqList;
+
+
+int main()
+{
+	
+	printf("hello");
+	return 0;
+}
