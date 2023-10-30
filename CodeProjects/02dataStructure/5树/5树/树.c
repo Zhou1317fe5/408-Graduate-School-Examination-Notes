5f4c2a4891d4531de7d6cffd16f1cb4e1fa9868e
Zhou Jiale <zjl256904767@outlook.com>
2023-10-30 20:40:17 +0800

    5二叉树

---
+++ CodeProjects/02dataStructure/5树/5树/树.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h> 
+#include <stdlib.h>
+
+//二叉树的链式存储
+typedef struct BiTNode {
+	int data; // 数据域
+	struct BiTNode* lchild, * rchild; // 左右孩子指针
+}BiTNode,*BiTree;
+
+BiTNode* root = NULL;
+root = (BiTNode*)malloc(sizeof(BiTNode));
+
+
